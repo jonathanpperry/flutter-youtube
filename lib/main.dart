7e83c02f74bbe50bb72bcb0529dbc6488e797f8f
Jonathan Perry <jpper@umich.edu>
2024-07-01 12:10:46 -0500

    Make changes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 // ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/firebase_options.dart';
 import 'package:youtube_clone/home_page.dart';
 
 void main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends ConsumerWidget {
